c6a32381dff61a9dd491b0328e669c6194fcdf9c
abdullah alrawi <abdullah.alrawe@student.ucll.be>
2024-05-21 15:38:11 +0200

    conf

--- src/main/java/be/groep14/conf/Configurations.java
+++ src/main/java/be/groep14/conf/Configurations.java
@@ -4,7 +4,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -17,8 +17,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -31,18 +29,32 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 @EnableWebSecurity
 public class Configurations implements WebMvcConfigurer {
 
-
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
 
-    @Bean // Turn off security for certain URLs
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/css/**", "/api/**", "/error").requestMatchers(antMatcher("/h2-console/**")); // https://stackoverflow.com/questions/74680244/h2-database-console-not-opening-with-spring-security;
-    }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers(antMatcher(HttpMethod.POST, "/api/admin")).denyAll()
+                                .requestMatchers(antMatcher("/h2-console/**")).permitAll()
+                                .requestMatchers(antMatcher("/api/**")).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin()
+                .and()
+                .httpBasic();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        return http.build();
+    }
     @Bean
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
@@ -64,4 +76,3 @@ public class Configurations implements WebMvcConfigurer {
         registry.addMapping("/**").allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS").allowedOrigins("*");
     }
 }
-
